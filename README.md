553154a201b29c90ad74577a2f557e14605af4d7
webOps-aman <amanhxe@gmail.com>
2024-12-24 19:23:43 +0530

    update work in 1_navbar

---
+++ README.md
@@ -0,0 +1,21 @@
+| Main Folder | Sub Folder | Description                                                         |
+|-------------|------------|---------------------------------------------------------------------|
+| navbars     | 1_navbar   | Responsive navbar built with Bootstrap and React for functionality. |
+|             |            |                                                                     |
+|             |            |                                                                     |
+|             |            |                                                                     |
+|             |            |                                                                     |
+|             |            |                                                                     |
+|             |            |                                                                     |
+|             |            |                                                                     |
+|             |            |                                                                     |
+|             |            |                                                                     |
+|             |            |                                                                     |
+|             |            |                                                                     |
+|             |            |                                                                     |
+|             |            |                                                                     |
+|             |            |                                                                     |
+|             |            |                                                                     |
+|             |            |                                                                     |
+|             |            |                                                                     |
+|             |            |                                                                     |
--- navbars/1_navbar/frontend/src/App.js
+++ navbars/1_navbar/frontend/src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className='navbar-section'>
+        <div className='container-fluid navbar-container'>
+          <div className='row navbar-row'>
+            <div className='col-md-12'>
+{/* ----------navbar start here---------- */}
+                <nav className="navbar navbar-expand-lg  fixed-top">
+                    <div className="container">
+                        <div className="offset-md-1">
+                            <a className="navbar-brand" href="#">Brand Name</a>
+                        </div>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0 offset-md-5">
+                                <li className="nav-item">
+                                    <a className="nav-link" aria-current="page" href="#">Home</a>
+                                </li>
+                                <li className="nav-item me-2">
+                                    <a className="nav-link" href="#">About Us</a>
+                                </li>
+                                <li className="nav-item me-2">
+                                    <a className="nav-link" href="#">Services</a>
+                                </li>
+                                <li className="nav-item me-2">
+                                    <a className="nav-link" href="#">Portfolio</a>
+                                </li>
+                                <li className="nav-item me-2">
+                                    <a className="nav-link" href="#">Team</a>
+                                </li>
+                                <li className="nav-item me-2">
+                                    <a className="nav-link" href="#">Contact Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+{/* ----------navbar end here---------- */}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
