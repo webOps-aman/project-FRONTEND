6e26cd2d6c64d22d8e580ad8b5327715d3f42593
webOps-aman <amanhxe@gmail.com>
2025-01-16 16:58:50 +0530

    about section completed

--- project-REACT/5_portfolio/frontend/src/components/About.jsx
+++ project-REACT/5_portfolio/frontend/src/components/About.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
     <>
-      <div id="about" className='about-section w-[100%] h-[87vh] mt-[1px] flex justify-center items-center' style={{backgroundImage: "url('/images/background-10.jpg')", backgroundSize: "cover", backgroundPosition: "center",  width: "100%"}}>
-        <div className='about-container w-[90%] h-[90%]  bg-black rounded-2xl'>
+      <div id="about" className='about-section sm:w-[100%] sm:h-[87vh] mt-[1px] sm:flex sm:justify-center sm:items-center' style={{backgroundImage: "url('/images/background-10.jpg')", backgroundSize: "cover", backgroundPosition: "center",  width: "100%"}}>
+        <div className='about-container sm:w-[90%] sm:h-[90%]  bg-black sm:rounded-2xl'>
 {/* -----about-content-start-here----- */}
-            <div className='about-content-area w-[95%] h-[100%] mx-auto rounded-2xl flex justify-around items-center border-2 border-solid border-white'>
+            <div className='about-content-area sm:w-[100%] sm:h-[100%] mx-auto sm:rounded-2xl sm:flex sm:justify-around sm:items-center'>
 
-                <div className='about-content-left w-[40%]'>
+                <div className='about-content-left sm:w-[20%]'>
                   <div className='about-title'>
-                    <p className='text-white text-9xl font-bold about' >About</p>
-                    <p className='text-white text-9xl font-bold'>me...</p>
+                    <p className='text-white sm:text-8xl text-3xl text-center sm:py-0 py-4 font-bold about' >About<br/> me...</p>
+
                   </div>  
                 </div> 
 
-                <div className='about-content-right w-[40%]'>
-                  <div className='about-details'>
-                    <p className='text-white text-2xl'>
-                      I am a Full Stack Developer with 1.5 years of experience in PHP development and a keen interest in the MERN stack. While I am a fresher in MERN, I have a solid foundation in web development and have been actively learning and building projects to strengthen my skills in React, Node.js, Express, and MongoDB. I am eager to contribute to impactful projects and grow as a MERN stack developer.
-                    </p>
+                <div className='about-content-right sm:w-[60%] flex justify-center items-center bg-gradient-to-r from-yellow-300 to-neutral-50 sm:rounded-3xl'>
+                  <div className='inner-box sm:w-[99%] w-[100%] sm:p-10 p-8 sm:rounded-3xl bg-black'>
+                      <div className='about-info'>
+                        <p className='text-white text-3xl font-medium text-center sm:pb-6 pb-4 italic'>I can develop that help people</p>
+                        <p className='text-white sm:text-2xl text-lg font-medium leading-8'>I am a Full Stack Developer with 1.5 years of experience in PHP development and a keen interest in the MERN stack. While I am a fresher in MERN, I have a solid foundation in web development and have been actively learning and building projects to strengthen my skills in React, Node.js, Express, and MongoDB. I am eager to contribute to impactful projects and grow as a MERN stack developer.</p>
+                      </div>
                   </div>
                 </div>       
 
