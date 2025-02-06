22aae15626a761527e88660a60ab73bcb8f17a91
webOps-aman <amanhxe@gmail.com>
2025-02-06 17:11:26 +0530

    added animation background herosection

---
+++ project-REACT/5_portfolio/frontend/src/api/myProjects.js
@@ -0,0 +1,35 @@
+export const myProjects = [
+    {
+        id: "1",
+        name: "Speech Text Convertor",
+        description: "A React-based application that seamlessly converts spoken words into text, enhancing accessibility and productivity.",
+        image: "https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80",
+        demoLink: "",
+        githubLink: ""
+    },
+    {
+        id: "2",
+        name: "ChatBot",
+        description: "A JavaScript-based assistant using Google Gemini API, delivering intelligent, real-time responses for seamless and interactive communication.",
+        image: "https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80",
+        demoLink: "",
+        githubLink: ""
+    },
+    {
+        id: "3",
+        name: "Responsive Sidebar",
+        description: "A React-based dynamic sidebar with seamless navigation, optimized for all screen sizes and enhanced user experience.",
+        image: "https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80",
+        demoLink: "",
+        githubLink: ""
+    },
+    {
+        id: "4",
+        name: "testing",
+        description: "testing",
+        image: "https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80",
+        demoLink: "",
+        githubLink: ""
+    },
+    
+];
--- project-REACT/5_portfolio/frontend/src/components/Projects.jsx
+++ project-REACT/5_portfolio/frontend/src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { myProjects } from '../api/myProjects'
 
 const Projects = () => {
   return (
@@ -22,75 +23,31 @@ const Projects = () => {
             <div className='project-card-section w-[95%] mx-auto'>
               <div className='sm:grid sm:grid-rows-2 sm:grid-cols-3 grid grid-cols-1'>
 {/* -----project-card-start-here----- */}
-              <div className="w-[95%] sm:mb-0 mb-5 mx-auto flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
-                <img 
-                  className="w-full h-auto rounded-t-xl" 
-                  src="https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80" 
-                  alt="Beautiful landscape with mountains and sunset"
-                />
-                <div className="p-4 md:p-5">
-                    <h3 className="text-lg font-bold text-gray-800 dark:text-white">Speech Text Convertor</h3>
-                    <p className="mt-1 text-gray-500 dark:text-neutral-400">
-                    A React-based application that seamlessly converts spoken words into text, enhancing accessibility and productivity.
-                    </p>
-                    
-                    <div className='w-[200px] button-group flex justify-between'>
-                      <div className='live-demo-div'>
-                        <button className='mt-2 py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none'>Live Demo</button>
-                      </div>
-                      <div className='github-div'>
+            {myProjects.slice(0, 3).map((project) => (
+              <div key={project.id} className="w-[95%] sm:mb-0 mb-5 mx-auto flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
+              <img 
+                className="w-full h-auto rounded-t-xl" 
+                src="https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80" 
+                alt="Beautiful landscape with mountains and sunset"
+              />
+              <div className="p-4 md:p-5">
+                  <h3 className="text-lg font-bold text-gray-800 dark:text-white">{project.name}</h3>
+                  <p className="mt-1 text-gray-500 dark:text-neutral-400">
+                    {project.description}
+                  </p>
+                  
+                  <div className='w-[200px] button-group flex justify-between'>
+                    <div className='live-demo-div'>
+                      <button className='mt-2 py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none'>Live Demo</button>
+                    </div>
+                    <div className='github-div'>
                       <button className='mt-2 py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none'>Github Link</button>
-                      </div>
                     </div>
                   </div>
-              </div>
-
-              <div className="w-[95%] sm:mb-0 mb-5 mx-auto flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
-                <img 
-                  className="w-full h-auto rounded-t-xl" 
-                  src="https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80" 
-                  alt="Beautiful landscape with mountains and sunset"
-                />
-                <div className="p-4 md:p-5">
-                    <h3 className="text-lg font-bold text-gray-800 dark:text-white">ChatBot</h3>
-                    <p className="mt-1 text-gray-500 dark:text-neutral-400">
-                    A JavaScript-based assistant using Google Gemini API, delivering intelligent, real-time responses for seamless and interactive communication.
-                    </p>
-                    
-                    <div className='w-[200px] button-group flex justify-between'>
-                      <div className='live-demo-div'>
-                        <button className='mt-2 py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none'>Live Demo</button>
-                      </div>
-                      <div className='github-div'>
-                      <button className='mt-2 py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none'>Github Link</button>
-                      </div>
-                    </div>
-                  </div>
-              </div>
-
-              <div className="w-[95%] mx-auto flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
-                <img 
-                  className="w-full h-auto rounded-t-xl" 
-                  src="https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80" 
-                  alt="Beautiful landscape with mountains and sunset"
-                />
-                <div className="p-4 md:p-5">
-                    <h3 className="text-lg font-bold text-gray-800 dark:text-white">Responsive Sidebar</h3>
-                    <p className="mt-1 text-gray-500 dark:text-neutral-400">
-                    A React-based dynamic sidebar with seamless navigation, optimized for all screen sizes and enhanced user experience.
-                    </p>
-                    
-                    <div className='w-[200px] button-group flex justify-between'>
-                      <div className='live-demo-div'>
-                        <button className='mt-2 py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none'>Live Demo</button>
-                      </div>
-                      <div className='github-div'>
-                      <button className='mt-2 py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none'>Github Link</button>
-                      </div>
-                    </div>
-                  </div>
-              </div>
-
+                </div>
+            </div>
+            ))}
+              
 {/* -----project-card-end-here----- */}
               </div>
 
